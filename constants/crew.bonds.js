ae05940a7f9a5435d4b869f9c53fff088ca33029
chavesjoabe <chavesjoabe1@gmail.com>
2023-10-13 13:12:38 -0300

    feat: create sort ministry method and tests

---
+++ constants/crew.bonds.js
@@ -0,0 +1,4 @@
+export const crewBonds = [
+  { name: 'Dener', bondName: 'Ester' },
+  { name: 'Tiago', bondName: 'Nathali' },
+]
--- services/crew.service.js
+++ services/crew.service.js
@@ -1,22 +1,23 @@
 import { eachDayOfInterval, endOfMonth, isSunday, isThursday, isTuesday, startOfMonth } from "date-fns";
 import { roles } from "../constants/service.constants.js";
+import { crewBonds } from "../constants/crew.bonds.js";
 
 export class CrewService {
   static formatDate(date) {
     return new Intl.DateTimeFormat('pt-br').format(date);
   }
 
-  static createServiceCrew(currentCrew, lastCrewArray = []) {
+  static createServiceCrew(currentCrew, lastCrew = {}) {
     const INEXISTENT_USER = 'empty';
     const userRoles = Object.values(roles);
     const serviceCrew = userRoles.map(
-      role => this.findPlayerByRole(currentCrew, role, lastCrewArray)
+      role => this.findPlayerByRole(currentCrew, role, lastCrew)
     );
 
     const filteredServiceCrew = serviceCrew.filter(([key, value]) => value !== INEXISTENT_USER);
 
     const response = Object.fromEntries(filteredServiceCrew);
-    const extraParticipant = this.getExtraParticipant(response, currentCrew, lastCrewArray);
+    const extraParticipant = this.getExtraParticipant(response, currentCrew, lastCrew);
     const bonds = this.applyUserBonds(currentCrew, response[roles.GUITAR_PLAYER]);
     if (extraParticipant) {
       response.extra = extraParticipant;
@@ -24,11 +25,30 @@ export class CrewService {
 
     if (!bonds) return response;
     response[bonds.role] = bonds.name;
+    response.ministry = this.sortServiceDayMinistry(currentCrew, lastCrew, response);
     return response;
   }
 
-  static sortServiceDayMinistry(currentCrew, lastCrew) {
-    return true;
+  static sortServiceDayMinistry(currentCrew, lastCrew, crewPreview) {
+    const bondUser = crewPreview[roles.GUITAR_PLAYER];
+    const ministersToRemove = crewBonds
+      .filter(user => user.name !== bondUser)
+      .map(item => item.bondName);
+
+    const ministers = currentCrew.filter(user => user.role === roles.MINISTRY);
+    if (ministers.length === 1) {
+      return ministers[0].name;
+    }
+    const lastCrewUserNames = Object.values(lastCrew);
+    const filteredMinisters = lastCrewUserNames.length === 0 
+      ? ministers
+      : ministers.filter(ministry => !lastCrewUserNames.includes(ministry.name));
+    const ministersWithoutBonds = filteredMinisters.filter(ministry => !ministersToRemove.includes(ministry.name));
+    if (ministersWithoutBonds.length === 0) {
+      return currentCrew.ministry;
+    }
+    const response = ministersWithoutBonds[this.getRandomElement(ministersWithoutBonds)].name;
+    return response;
   }
 
   static getExtraParticipant(userCrew, currentUserList, lastUserList = {}) {
@@ -69,7 +89,7 @@ export class CrewService {
     const NOT_FOUND_VALUE = -1;
     const usersByRole = userArray.filter(user => user.role === role);
     const lastCrewUserByRole = lastUserCrew[role];
-    
+
     if (usersByRole.length === 0) {
       return [role, 'empty'];
     }
